807b800ef4f620196ac404ae3b3d9c57b2ef5eba
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2025-06-19 11:08:41 +0530

    init_store_uitls

---
+++ src/utils/getTheme.js
@@ -0,0 +1,6 @@
+export const getTheme = (theme) => {
+  if (theme === "red") return "text-red-600 border-red-600";
+  if (theme === "orange") return "text-orange-400 border-orange-400";
+  if (theme === "green") return "text-green-600 border-green-600";
+    if (theme === "white") return "text-white border-white";
+};
