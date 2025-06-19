86e38753a53d6c9631132433cb8c356206f48786
MOHAMMED-IQRAMUL <MOHDIQRAMUL2468@GMAIL.COM>
2025-06-19 11:08:28 +0530

    init_constants

---
+++ src/constant/ProjectData.js
@@ -0,0 +1,165 @@
+export const ProjectData =  [
+    {
+      Title: "AI Flashcard Generator",
+      Technology: ["Next.js", "Firebase", "Clerk", "Stripe", "Material UI"],
+      Description:
+        "FlashLearn is an AI-powered flashcard generation web application that allows users to create, store, and review flashcards.",
+      Features: [
+        "Generate Flashcards",
+        "User Authentication",
+        "Save and View Flashcards",
+      ],
+      GitLink: "https://github.com/MOHAMMED-IQRAMUL/AI-FLASHCARD-GENERATOR",
+      LiveDemoLink: "https://aiflashlearn.vercel.app/",
+      Image: "src\\assets\\AIFLASHCARDGENERATOR.png",
+      ImageAlt: "AI Flashcard Generator",
+    },
+    {
+      Title: "Full Stack Realtime Chat App",
+      Technology: [
+        "React",
+        "Zustand",
+        "Axios",
+        "Socket.IO",
+        "Client",
+        "DaisyUI",
+        "bcryptjs",
+        "cloudinary",
+        "cookie-parser",
+        "express",
+        "jsonwebtoken",
+        "mongoose",
+      ],
+      Description:
+        "This application is designed for seamless real-time communication",
+      Features: [
+        "Real-Time Messaging",
+        "Authentication & Authorization",
+        "Modern Tech Stack",
+      ],
+      GitLink: "https://github.com/MOHAMMED-IQRAMUL/ChatApp",
+      LiveDemoLink: "https://chatapp-9kwj.onrender.com/",
+      Image:
+        "https://github.com/MOHAMMED-IQRAMUL/ChatApp/raw/main/frontend/public/chats.png",
+      ImageAlt: "Full Stack Realtime Chat App",
+    },
+  
+    {
+      Title: "Smart Inventory Management",
+      Technology: ["Next.js", "Firebase"],
+      Description:
+        "A Clean CRUD app that allows users to add, remove, edit, and delete items. It also enables searching for recipes based on available inventory items.",
+      Features: ["Fast", "Responsive", "AI Support"],
+      GitLink: "https://github.com/MOHAMMED-IQRAMUL/Smart-Inventory-Management",
+      LiveDemoLink: "https://smart-inventory-six.vercel.app",
+      Image: "src\\assets\\SmartInventoryManagement.png",
+      ImageAlt: "Smart Inventory Management",
+    },
+    {
+      Title: "AI Chat Support",
+      Technology: ["OpenRouter Llama-3", "JavaScript"],
+      Description:
+        "An AI-powered assistant designed to help users with various inquiries and tasks.",
+      Features: ["Dynamic", "Responsive", "User Friendly"],
+      GitLink: "https://github.com/MOHAMMED-IQRAMUL/AI-Chat-Support",
+      LiveDemoLink: "https://ai-support-buddy.vercel.app",
+      Image: "src\\assets\\AIChatSupport.png",
+      ImageAlt: "AI Chat Support",
+    },
+    {
+      Title: "AI Rate My Professor",
+      Technology: ["Next.js", "Firebase", "OpenAI", "Pinecone", "RAG"],
+      Description:
+        "An AI-powered support assistant that leverages a RAG system to help students easily find and evaluate professors based on their specific needs and preferences.",
+      Features: [
+        "AI-Powered Retrieval",
+        "Personalized Recommendations",
+        "Search for Professors",
+      ],
+      GitLink:
+        "https://github.com/MOHAMMED-IQRAMUL/Rate-My-Professor-Using-RAG",
+      LiveDemoLink: "https://professai.vercel.app",
+      Image: "src\\assets\\AiRMP.png",
+      ImageAlt: "AI Rate My Professor",
+    },
+    {
+      Title: "Calendar Project",
+      Technology: ["HTML", "CSS", "JavaScript"],
+      Description:
+        "A simple and dynamic calendar created using HTML, CSS, and JavaScript.",
+      Features: ["Dynamic", "Responsive", "User Friendly"],
+      GitLink: "https://github.com/MOHAMMED-IQRAMUL/Calendar-Project",
+      LiveDemoLink: "https://calender-iqramul.netlify.app",
+      Image: "https://github.com/MOHAMMED-IQRAMUL/CALENDER/raw/main/IMG/Screenshot%202024-01-19%20074324.png",
+      ImageAlt: "Calendar Project",
+    },
+    {
+      Title: "React TodoList App",
+      Technology: ["React", "Tailwind CSS", "React Icons"],
+      Description:
+        "A simple React TodoList application that can be used as a notepad.",
+      Features: [
+        "Simple TodoList App",
+        "Responsive",
+        "Managing List with Local Storage",
+      ],
+      GitLink: "https://github.com/MOHAMMED-IQRAMUL/React-TodoList-App",
+      LiveDemoLink: "https://iq-task.netlify.app",
+      Image: "https://private-user-images.githubusercontent.com/153977920/317659907-7e3a50d6-b000-4428-ba64-58ee4af857ff.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDcxNDI3NDMsIm5iZiI6MTc0NzE0MjQ0MywicGF0aCI6Ii8xNTM5Nzc5MjAvMzE3NjU5OTA3LTdlM2E1MGQ2LWIwMDAtNDQyOC1iYTY0LTU4ZWU0YWY4NTdmZi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNTEzJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDUxM1QxMzIwNDNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0yYjc1ODdmYzY4YmFhZWYyY2U1MDY3OWJhNTM2NzEwYmMyNWJkZWE3Mjc3ZjQzZWU4NzFkYTk3NDhmYTc0MDY0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.j_k1N35gGxEcvksqoFkjRb825UGmqWyOff6Lxfw4yTQ",
+      ImageAlt: "React TodoList App",
+    },
+    {
+      Title: "EduTech Web App",
+      Technology: ["Next.js", "Material UI", "Tailwind CSS", "OpenAI", "Clerk"],
+      Description:
+        "Building a bridge of education with features like note summarizer, understanding and evaluation, centralized dashboard, and AI-powered chat assistant.",
+      Features: [
+        "Note Summarizer",
+        "Understanding and Evaluation",
+        "Centralized Dashboard",
+        "AI-Powered Chat Assistant",
+      ],
+      GitLink: "https://github.com/MOHAMMED-IQRAMUL/EduTech-Web-App",
+      LiveDemoLink: "https://vidyarthigram.vercel.app",
+      Image: "src\\assets\\Vidyarthigram.png",
+      ImageAlt: "EduTech Web App",
+    },
+    {
+      Title: "NotesApp",
+      Technology: [
+        "React",
+        "Redux Toolkit",
+        "React Router",
+        "Tailwind CSS",
+        "Vite",
+      ],
+      Description:
+        "A simple yet powerful note-taking app that stores your notes in local storage. You can easily add, update, delete, copy, and manage your notes with undo and redo functionality.",
+      Features: [
+        "Home Page: Add or update notes.",
+        "Notes Page: Display all notes.",
+        "Specific Note Page: Render specific notes.",
+      ],
+      GitLink: "https://github.com/MOHAMMED-IQRAMUL/NotesApp",
+      LiveDemoLink: "https://iq-notes.netlify.app",
+      Image: "https://private-user-images.githubusercontent.com/153977920/405248496-424a4b90-da79-4bb9-b0e9-a14c1c476c4f.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDcxNDI3NDYsIm5iZiI6MTc0NzE0MjQ0NiwicGF0aCI6Ii8xNTM5Nzc5MjAvNDA1MjQ4NDk2LTQyNGE0YjkwLWRhNzktNGJiOS1iMGU5LWExNGMxYzQ3NmM0Zi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNTEzJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDUxM1QxMzIwNDZaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iZTBmMTMzYzgwN2FhYWViNWJmMzg2OWNmNjU1ZTU3ZjZmZGM3MDMxNzE4ZTM3ZWRlNTQ3NTcxZGJjOWI5Nzc0JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.3PDvbGZyjmJYWMLuSVpBSqP-zF6Vq8_JDC6SY9TZoJg",
+      ImageAlt: "NotesApp",
+    },
+    {
+      Title: "FarmRuler (Ongoing)",
+      Technology: ["Next.js", "Tailwind CSS", "Material UI", "Vercel"],
+      Description:
+        "A simple and clean UI to display weather conditions in specific regions. Create personalized AI bots. Monitor crop health, growth stages, and harvest cycles to optimize yield.",
+      Features: [
+        "Track Your Crops",
+        "Analyze Farm Performance",
+        "Weather Forecasts",
+        "Manage Your Livestock",
+      ],
+      GitLink: "https://github.com/MOHAMMED-IQRAMUL/FarmRuler",
+      LiveDemoLink: "https://farm-ruler.vercel.app",
+      Image: "src\\assets\\FarmRuler.png",
+      ImageAlt: "FarmRuler",
+    },
+    
+  ];
