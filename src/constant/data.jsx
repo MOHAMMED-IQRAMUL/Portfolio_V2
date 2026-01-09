8fd9e3e37129c2c64f5c7a9fd0b09456e4769a21
MOHAMMED IQRAMUL <153977920+MOHAMMED-IQRAMUL@users.noreply.github.com>
2026-01-09 12:13:43 +0530

    Update data.jsx

--- src/constant/data.jsx
+++ src/constant/data.jsx
@@ -1,6 +1,6 @@
 import Leetcode from "../components/Leetcode.jsx";
 import LinkedIn from "../components/LinkedInIcon.jsx";
-// export const LeftSideBarData = ['GitHub', 'LinkedIN', 'LeetCode', 'Resume']
+// export const LeftSideBarData = ['GitHub', 'LinkedIN', 'LeetCode']
 export const LeftSideBarData = [
 
   {
@@ -31,24 +31,7 @@ export const LeftSideBarData = [
     svg: < Leetcode />,
     url: "https://www.leetcode.com/mohammed-iqramul/"
   },
-  {
-    Name: "Resume",
-    svg: (
-      <svg
-        stroke="currentColor"
-        fill="white"
-        strokeWidth="0"
-        viewBox="0 0 640 512"
-
-        height="30"
-        width="30"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M48 0C21.53 0 0 21.53 0 48v64c0 8.84 7.16 16 16 16h80V48C96 21.53 74.47 0 48 0zm208 412.57V352h288V96c0-52.94-43.06-96-96-96H111.59C121.74 13.41 128 29.92 128 48v368c0 38.87 34.65 69.65 74.75 63.12C234.22 474 256 444.46 256 412.57zM288 384v32c0 52.93-43.06 96-96 96h336c61.86 0 112-50.14 112-112 0-8.84-7.16-16-16-16H288z"></path>
-      </svg>
-    ),
-    url:"https://drive.google.com/file/d/1fLxgTFKkOZHUddNTPdAy-s_kXpfOkhwi/view?usp=sharing"
-  },
+ 
 ];
 
 export const TechnicalProficiency = [
